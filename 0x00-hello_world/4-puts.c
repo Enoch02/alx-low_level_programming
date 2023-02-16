07eb61a0835fd3bc0e62f5b9d7946381610ecc71
Enoch02 <adesanyaenoch@gmail.com>
2023-02-16 06:52:19 -0800

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * Description: Prints text to stdout with puts
+ */
+int main(void)
+{
+	char text[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(text);
+	return (0);
+}
+
