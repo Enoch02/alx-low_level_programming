3b797675200c39338b3e93aa4347fd1b3192627f
Enoch02 <adesanyaenoch@gmail.com>
2023-02-17 12:07:09 -0800

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - The main function
+ * Description: print all possible combination of single digits
+ * Return: Returns 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+			putchar(',');
+			putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
