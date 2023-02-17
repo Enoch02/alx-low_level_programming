9ceca7a04e9a8e5d39d7411590d3d769b1437d7a
Enoch02 <adesanyaenoch@gmail.com>
2023-02-17 11:25:11 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - The main function
+ * Description: prints 0 - 9
+ * Return: Returns 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (int i = 0; i < 10; i++)
+		printf("%d", i);
+
+	printf("\n");
+	return (0);
+}
