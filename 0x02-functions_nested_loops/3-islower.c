3ecc872754ed8b10cc240677d001ecae86bb045e
Enoch02 <adesanyaenoch@gmail.com>
2023-02-21 13:11:53 +0100

    Task 3.1

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,35 +1,4 @@
-#include <stdio.h>
-
-/**
- * _islower - Checks letters case
- * @c: the letter to check
- *
- * Description: Checks if a letter is lowercase
- * Return: Returns 0 if false
- */
-int _islower(int c);
-
-/**
- * main - The main function
- * Description: calls _islower
- * Return: Returns 0
- */
-int main(void)
-{
-	int r;
-
-	r = _islower('H');
-	putchar(r + '0');
-
-	r = _islower('o');
-
-	putchar(r + '0');
-	r = _islower(108);
-	putchar(r + '0');
-	putchar('\n');
-
-	return (0);
-}
+#include "main.h"
 
 /**
  * _islower - Checks letters case
