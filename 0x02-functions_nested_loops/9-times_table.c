38ff20dd26d20972320e88758a670a70003f3d89
Enoch02 <adesanyaenoch@gmail.com>
2023-02-21 17:35:00 +0100

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * times_table - Prints a times table
+ * Description: Prints a 9 times table
+ * Return: Returns 0
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			int product = i * j;
+
+			if (product < 10)
+			{
+				_putchar(product + '0');
+
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+			}
+
+			else if (product >= 10)
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+
+		}
+		_putchar('\n');
+	}
+}
