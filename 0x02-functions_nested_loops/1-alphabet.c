6c3cd45565bb2d35ced755490e22680e870822c5
Enoch02 <adesanyaenoch@gmail.com>
2023-02-21 00:15:45 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - Prints letters
+ * Description: Prints lowercase letters
+ * Return: Returns 0
+ */
+void print_alphabet(void);
+
+/**
+ * main - The main function
+ * Description: Calls print_alphabet
+ * Return: Returns 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+/**
+ * print_alphabet - Prints letters
+ * Description: Prints lowercase letters
+ * Return: Returns 0
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
