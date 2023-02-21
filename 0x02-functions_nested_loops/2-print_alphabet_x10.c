c72d38d94ffcbd75c0f0ad0cbd27d1c4c6b51849
Enoch02 <adesanyaenoch@gmail.com>
2023-02-21 13:06:28 +0100

    Task 2.1

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,3 @@
-#include <stdio.h>
-
-/**
-  * print_alphabet_x10 - Prints letters
-  * Description: Prints lowercase letters 10 times
-  * Return: Returns 0
-  */
-void print_alphabet_x10(void);
-
-/**
- * main - The main function
- * Description: Calls print_albhabet_x10
- * Return: Returns 0
- */
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
-
 /**
   * print_alphabet_x10 - Prints letters
   * Description: Prints lowercase letters 10 times
