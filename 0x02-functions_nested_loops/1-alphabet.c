487960a0e6d7317be6bfc5673bd03e5a1705fcca
Enoch02 <adesanyaenoch@gmail.com>
2023-02-21 12:38:42 +0100

    Task 1.2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,6 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints letters
- * Description: Prints lowercase letters
- * Return: Returns 0
- */
-void print_alphabet(void);
-
-/**
  * main - The main function
  * Description: Calls print_alphabet
  * Return: Returns 0
@@ -16,20 +9,4 @@ int main(void)
 {
 	print_alphabet();
 	return (0);
-}
-
-/**
- * print_alphabet - Prints letters
- * Description: Prints lowercase letters
- * Return: Returns 0
- */
-void print_alphabet(void)
-{
-	char i;
-
-	for (i = 97; i < 123; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
 }
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -12,3 +12,19 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+/**
+ * print_alphabet - Prints letters
+ * Description: Prints lowercase letters
+ * Return: Returns 0
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
