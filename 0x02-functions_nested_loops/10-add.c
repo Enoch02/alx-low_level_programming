e649bcfc27c1ffc92c2cb354e7e42ae207b1c653
Enoch02 <adesanyaenoch@gmail.com>
2023-02-21 17:56:05 +0100

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Adds numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Description: Adds 2 numbers to each other
+ * Return: Returns the result of the addition
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,16 @@
 #include "main.h"
 
+int main(void)
+{
+    times_table();
+    return (0);
+}
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
 /**
  * times_table - Prints a times table
  * Description: Prints a 9 times table
@@ -12,20 +23,34 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		_putchar('0');
-
 		for (j = 0; j < 10; j++)
 		{
 			int product = i * j;
 
-			_putchar(',');
-			_putchar(' ');
+			if (product < 10)
+			{
+				_putchar(product + '0');
 
-			if (product <= 9)
-				_putchar(' ');
-			else
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+			}
+
+			else if (product >= 10)
+			{
 				_putchar((product / 10) + '0');
-			_putchar((product % 10) + '0');
+				_putchar((product % 10) + '0');
+
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+
 		}
 		_putchar('\n');
 	}
