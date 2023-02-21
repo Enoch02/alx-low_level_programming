c2af2a1c3770ab248eea9bb51ea7930d0af3e197
Enoch02 <adesanyaenoch@gmail.com>
2023-02-21 13:15:59 +0100

    Task 4.1

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,37 +1,4 @@
-#include <stdio.h>
-
-/**
- * _isalpha - Checks letters case
- * @c: the letter to check
- *
- * Description: Checks for alphabetic characters
- * Return: Returns 0 if false
- */
-int _isalpha(int c);
-
-/**
- * main - The main function
- * Description: calls _isalpha
- * Return: Returns 0
- */
-int main(void)
-{
-	int r;
-
-	r = _isalpha('H');
-	putchar(r + '0');
-
-	r = _isalpha('o');
-	putchar(r + '0');
-
-	r = _isalpha(108);
-	putchar(r + '0');
-
-	r = _isalpha(';');
-	putchar(r + '0');
-	putchar('\n');
-	return (0);
-}
+#include "main.h"
 
 /**
  * _isalpha - Checks letters case
@@ -42,13 +9,15 @@ int main(void)
  */
 int _isalpha(int c)
 {
-	for (int i = 97; i < 123; i++)
+	int i;
+
+	for (i = 97; i < 123; i++)
 	{
 		if (i == c)
 			return (1);
 	}
 
-	for (int i = 65; i < 91; i++)
+	for (i = 65; i < 91; i++)
 	{
 		if (i == c)
 			return (1);
