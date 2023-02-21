f8bfa3a7ee7ab72aa1627b00b7dd1c9a05960d06
Enoch02 <adesanyaenoch@gmail.com>
2023-02-21 17:02:48 +0100

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,29 +17,22 @@ int _putchar(char c)
  * Return: Returns 0
  */
 void jack_bauer(void)
-{
+{ 
 	char i;
 	char j;
-
-	_putchar('0');
-	_putchar(i + '0');
-	_putchar(':');
-	_putchar('0');
-	_putchar(k + '0');
 
 	for (i = 0; i < 24; i++)
 	{
 		for (j = 0; j < 60; j++)
 		{
-			if (i < 10)
-				_putchar('0');
-			_putchar(i + '0');
-
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			
 			_putchar(':');
 
-			if (j < 10)
-				_putchar('0');
-			_putchar(j + '0');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			
 			_putchar('\n');
 		}
 	}
