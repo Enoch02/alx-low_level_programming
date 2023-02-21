64ba9bf7ea7fc9ff53569f6b608805089f8073cc
Enoch02 <adesanyaenoch@gmail.com>
2023-02-21 17:43:00 +0100

    Task 9.1

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,34 +12,20 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
+		_putchar('0');
+
 		for (j = 0; j < 10; j++)
 		{
 			int product = i * j;
 
-			if (product < 10)
-			{
-				_putchar(product + '0');
+			_putchar(',');
+			_putchar(' ');
 
-				if (j < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
-			}
-
-			else if (product >= 10)
-			{
+			if (product <= 9)
+				_putchar(' ');
+			else
 				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
-
-				if (j < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-
+			_putchar((product % 10) + '0');
 		}
 		_putchar('\n');
 	}
