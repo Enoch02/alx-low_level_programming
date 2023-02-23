f743f813c5fe29b88f3c9d4b8d2df618dae2f70d
Enoch02 <adesanyaenoch@gmail.com>
2023-02-23 12:57:02 +0100

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers
+ * Description: Prints from 0 to 9
+ * Return: Returns 0
+ */
+void print_numbers(void)
+{
+    char i;
+
+    for (i = 0; i < 10; i++)
+    {
+        _putchar(i + '0');    
+    }
+    _putchar('\n');
+}
