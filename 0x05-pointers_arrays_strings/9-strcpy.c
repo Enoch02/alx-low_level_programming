76840eb90661aa501370276cdc4c9bc17c334c4c
Enoch02 <adesanyaenoch@gmail.com>
2023-02-27 14:29:14 +0100

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - Copy string to another location
+ * @dest: Destination
+ * @src: Source
+ *
+ * Return: Returns nothing
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int src_len = _get_len(src);
+
+	for (i = 0; i < src_len && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	dest[i + 1] = '\0';
+
+	return (dest);
+}
+
+/**
+ * _get_len - Get length of a string
+ * @s: string to reverse
+ *
+ * Return: Returns length
+ */
+int _get_len(char *s)
+{
+	int i = 0;
+	int len = 0;
+
+	while (s[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+
+	return (len);
+}
