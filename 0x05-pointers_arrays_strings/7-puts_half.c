5bf113e3d326a84ae8529457eca338654a47e270
Enoch02 <adesanyaenoch@gmail.com>
2023-02-27 13:41:26 +0100

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * puts_half - Print half a string
+ * @str: string to print
+ *
+ * Return: Returns nothing
+ */
+void puts_half(char *str)
+{
+	int len = _get_len(str);
+	int i;
+	int n;
+
+	if (len % 2 == 0)
+		n = len / 2;
+	else
+		n = (len - 1) / 2;
+
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
+
+/**
+ * _get_len - Get length of a string
+ * @s: string to reverse
+ *
+ * Return: Returns length
+ */
+int _get_len(char *s)
+{
+	int i = 0;
+	int len = 0;
+
+	while (s[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+
+	return (len);
+}
