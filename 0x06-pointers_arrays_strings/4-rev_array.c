c87251c5336808cbafcde463581980fa3fec55d3
Enoch02 <adesanyaenoch@gmail.com>
2023-03-01 21:40:00 +0100

    Task 2

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -14,8 +14,9 @@ void reverse_array(int *a, int n)
 
     for (i = 0; i < n; i++)
     {
+        n--;
         temp = a[i];
-        a[i] = a[n - i];
-        a[n - i] = temp;
+        a[i] = a[n];
+        a[n] = temp;
     }
 }
