16c205c86c62d084b7744999860e4018ba24dab1
Enoch02 <adesanyaenoch@gmail.com>
2023-03-06 18:40:21 +0100

    Task 2.1

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strspn - Get the length of a prefix substring
+ * @s: String
+ * @accept: Substring to accept
+ *
+ * Return: Returns a the length of the substring
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+
+}
