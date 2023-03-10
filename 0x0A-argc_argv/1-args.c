f787a1ad0cc37b08610967f60a60d46585f84711
Enoch02 <adesanyaenoch@gmail.com>
2023-03-10 19:13:48 +0100

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, __attribute__((__unused__)) char **argv)
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
