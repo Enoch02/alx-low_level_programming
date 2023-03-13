057caaf37a69a7486348f25997dfe2c669de598e
Enoch02 <adesanyaenoch@gmail.com>
2023-03-13 10:05:09 +0100

    Task 0 fix fix fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,8 +15,10 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	
+
 	arr = malloc(sizeof(c) * size);
+	if (arr == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		arr[i] = c;
